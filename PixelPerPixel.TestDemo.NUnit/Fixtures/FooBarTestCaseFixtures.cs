022aa1dcad2437dad42b3253c32fa58f59dd94e0
Jonathan Thibault <Jothib@outlook.com>
2023-10-20 20:20:45 -0400

    first data driven test

---
+++ PixelPerPixel.TestDemo.NUnit/Fixtures/FooBarTestCaseFixtures.cs
@@ -0,0 +1,32 @@
+﻿using PixelPerPixel.TestDemo.Domain;
+
+namespace PixelPerPixel.TestDemo.UnitTests.NUnit.Fixtures
+{
+    public static class FooBarTestCaseFixtures
+    {
+        public static FooBar[] FooBarDataDrivenTestData()
+        {
+            return new[]
+            {
+                new FooBar
+                {
+                    Foo = 1,
+                    Bar = "1"
+                },
+
+                new FooBar
+                {
+                    Foo = 2,
+                    Bar = "2"
+                },
+
+                new FooBar
+                {
+                    Foo = 3,
+                    Bar = "3"
+                }
+            };
+
+        }
+    }
+}
--- PixelPerPixel.TestDemo.NUnit/FooBarServiceUnitTests.cs
+++ PixelPerPixel.TestDemo.NUnit/FooBarServiceUnitTests.cs
@@ -1,3 +1,4 @@
+using PixelPerPixel.TestDemo.Domain;
 using PixelPerPixel.TestDemo.Services;
 using PixelPerPixel.TestDemo.Services.Interfaces;
 using PixelPerPixel.TestDemo.UnitTests.NUnit.Fixtures;
@@ -9,6 +10,8 @@ namespace PixelPerPixel.TestDemo.UnitTests.NUnit
     {
         private FooBarRepositoryMocks repositoryMocks;
 
+        private static IEnumerable<FooBar> fooBarDataDrivenTestData;
+
         [SetUp]
         public void Setup()
         {
@@ -17,6 +20,8 @@ namespace PixelPerPixel.TestDemo.UnitTests.NUnit
             this.repositoryMocks.MockSaveFooBar();
 
             this.repositoryMocks.MockGetFooBar(FooBarFixture.Default);
+
+            fooBarDataDrivenTestData = FooBarTestCaseFixtures.FooBarDataDrivenTestData().ToList();
         }
 
         [Test]
@@ -30,6 +35,17 @@ namespace PixelPerPixel.TestDemo.UnitTests.NUnit
         }
 
         [Test]
+        [TestCaseSource(nameof(TestCaseFooBarFixture))]
+        public async Task DataDrivenSaveFooBarServiceTest(FooBar fooBar)
+        {
+            IFooBarService service = new FooBarService(this.repositoryMocks.RepositoryMock.Object);
+
+            var savedFooBar = await service.SaveFooBar(fooBar);
+
+            Assert.True(savedFooBar.Bar.EndsWith("abc"));
+        }
+
+        [Test]
         public async Task GetFooBarServiceTest()
         {
             IFooBarService service = new FooBarService(this.repositoryMocks.RepositoryMock.Object);
@@ -37,6 +53,28 @@ namespace PixelPerPixel.TestDemo.UnitTests.NUnit
             var savedFooBar = await service.GetFooBar(123);
 
             Assert.That(savedFooBar.Foo, Is.EqualTo(123));
+        }
+
+        private static IEnumerable<FooBar> TestCaseFooBarFixture()
+        {
+
+            yield return new FooBar
+            {
+                Foo = 1,
+                Bar = "1"
+            };
+
+            yield return new FooBar
+            {
+                Foo = 2,
+                Bar = "2"
+            };
+
+            yield return new FooBar
+            {
+                Foo = 3,
+                Bar = "3"
+            };
         }
     }
 }
